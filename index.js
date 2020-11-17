cd10b71583f66a218a44600b0734b6c27952aa02
asd2795asd <asd2795asd@gmail.com>
2020-11-17 18:05:17 +0800

    update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const images = ['fox1', 'fox2', 'fox3', 'fox4'];
-const imgElem = document.querySelector('img');
+// const images = ['fox1', 'fox2', 'fox3', 'fox4'];
+// const imgElem = document.querySelector('img');
 
-function randomValueFromArray(array) {
-  let randomNo = Math.floor(Math.random() * array.length);
-  return array[randomNo];
-}
+// function randomValueFromArray(array) {
+//   let randomNo = Math.floor(Math.random() * array.length);
+//   return array[randomNo];
+// }
 
 // setInterval(function () {
 //   let randomChoice = randomValueFromArray(images);
@@ -13,43 +13,47 @@ function randomValueFromArray(array) {
 
 // Register service worker to control making site work offline
 console.log('test', navigator)
-if ('serviceWorker' in navigator) {
-  console.log('準備建立server work')
-  navigator.serviceWorker
-    .register('sw.js')
-    .then(function () { console.log('Service Worker Registered'); })
-    .catch(function (err) { console.log('server建立失敗', err) })
-}
+window.addEventListener('load', function() {
+  
+  if ('serviceWorker' in navigator) {
+    console.log('準備建立server work')
+    navigator.serviceWorker
+      .register('sw.js')
+      .then(function () { console.log('Service Worker Registered'); })
+      .catch(function (err) { console.log('server建立失敗', err) })
+  }
 
-// Code to handle install prompt on desktop
+  // Code to handle install prompt on desktop
 
-let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
+  let deferredPrompt;
+  const addBtn = document.querySelector('.add-button');
+  addBtn.style.display = 'none';
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  console.log('beforeinstallprompt', e)
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
+  window.addEventListener('beforeinstallprompt', (e) => {
+    console.log('beforeinstallprompt', e)
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = 'block';
 
-  addBtn.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
-    // 顯示提示
-    deferredPrompt.prompt();
-    // 等待用戶響應提示
-    deferredPrompt.userChoice.then((choiceResult) => {
-      console.log('用戶接受', choiceResult)
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
+    addBtn.addEventListener('click', (e) => {
+      // hide our user interface that shows our A2HS button
+      addBtn.style.display = 'none';
+      // 顯示提示
+      deferredPrompt.prompt();
+      // 等待用戶響應提示
+      deferredPrompt.userChoice.then((choiceResult) => {
+        console.log('用戶接受', choiceResult)
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
     });
   });
-});
+
+})
